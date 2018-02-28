192adaee7285b237efa07e030e10a3ebbf44f37e
diego <diego@192.168.0.21>
2018-02-27 22:41:03 -0600

    Practica1 en proceso

---
+++ src/ipc1/practica1_201503384/Jugador.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipc1.practica1_201503384;
+/**
+ *
+ * @author diego
+ */
+public class Jugador {
+    private char simbolo;
+    private int num_jugador;
+
+    /**
+     * @return the simbolo
+     */
+    public char getSimbolo() {
+        return simbolo;
+    }
+
+    /**
+     * @param simbolo the simbolo to set
+     */
+    public void setSimbolo(char simbolo) {
+        this.simbolo = simbolo;
+    }
+
+    /**
+     * @return the num_jugador
+     */
+    public int getNum_jugador() {
+        return num_jugador;
+    }
+
+    /**
+     * @param num_jugador the num_jugador to set
+     */
+    public void setNum_jugador(int num_jugador) {
+        this.num_jugador = num_jugador;
+    }
+}
