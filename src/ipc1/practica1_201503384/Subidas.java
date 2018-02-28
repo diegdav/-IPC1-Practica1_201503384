218148f9ab75ee0908c93cf8301c3015f66e95b6
diego <diego@192.168.0.21>
2018-02-27 22:40:42 -0600

    Practica1 en proceso

---
+++ src/ipc1/practica1_201503384/Subidas.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ipc1.practica1_201503384;
+
+/**
+ *
+ * @author diego
+ */
+public class Subidas {
+    private int inicio_x[][];
+    private int inicio_y[][];
+    private int final_x[][];
+    private int final_y[][];
+
+    /**
+     * @return the inicio_x
+     */
+    public int[][] getInicio_x() {
+        return inicio_x;
+    }
+
+    /**
+     * @param inicio_x the inicio_x to set
+     */
+    public void setInicio_x(int[][] inicio_x) {
+        this.inicio_x = inicio_x;
+    }
+
+    /**
+     * @return the inicio_y
+     */
+    public int[][] getInicio_y() {
+        return inicio_y;
+    }
+
+    /**
+     * @param inicio_y the inicio_y to set
+     */
+    public void setInicio_y(int[][] inicio_y) {
+        this.inicio_y = inicio_y;
+    }
+
+    /**
+     * @return the final_x
+     */
+    public int[][] getFinal_x() {
+        return final_x;
+    }
+
+    /**
+     * @param final_x the final_x to set
+     */
+    public void setFinal_x(int[][] final_x) {
+        this.final_x = final_x;
+    }
+
+    /**
+     * @return the final_y
+     */
+    public int[][] getFinal_y() {
+        return final_y;
+    }
+
+    /**
+     * @param final_y the final_y to set
+     */
+    public void setFinal_y(int[][] final_y) {
+        this.final_y = final_y;
+    }
+    
+    
+}
