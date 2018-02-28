d96d8a80750f212bb2002a9fbae8436364da285b
diego <diego@192.168.0.21>
2018-02-28 01:07:23 -0600

    Practica1 en proceso

--- src/ipc1/practica1_201503384/Tablero.java
+++ src/ipc1/practica1_201503384/Tablero.java
@@ -10,5 +10,52 @@ package ipc1.practica1_201503384;
  * @author diego
  */
 public class Tablero {
-    
+
+    private int columnas;
+    private int filas;
+    private char[][] tablero;
+
+    /**
+     * @return the columnas
+     */
+    public int getColumnas() {
+        return columnas;
+    }
+
+    /**
+     * @param columnas the columnas to set
+     */
+    public void setColumnas(int columnas) {
+        this.columnas = columnas;
+    }
+
+    /**
+     * @return the filas
+     */
+    public int getFilas() {
+        return filas;
+    }
+
+    /**
+     * @param filas the filas to set
+     */
+    public void setFilas(int filas) {
+        this.filas = filas;
+    }
+
+    public void mostrarTablero() {
+        tablero = new char[getFilas()][getColumnas()];
+        
+        int fila, columna;
+        for (fila = 0; fila < filas; fila++) {
+            for (columna = 0; columna < columnas; columna++) {
+                System.out.print("|_");
+                tablero[fila][columna] = ' ';
+                System.out.print(tablero[fila][columna]);
+                System.out.print("_");
+            }
+            System.out.println("|");
+        }
+        System.out.println("");
+    }
 }
