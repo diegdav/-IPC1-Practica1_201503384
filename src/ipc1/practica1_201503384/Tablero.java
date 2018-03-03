6c883e99ae1499925c89146997d0ee495bd7f467
diego <diego@192.168.0.21>
2018-03-02 20:44:52 -0600

    Practica1 en proceso

--- src/ipc1/practica1_201503384/Tablero.java
+++ src/ipc1/practica1_201503384/Tablero.java
@@ -13,7 +13,11 @@ public class Tablero {
 
     private int columnas;
     private int filas;
-    private char[][] tablero;
+    Subidas[] subida;
+    private String[][] mostrar_tablero;
+
+    public Tablero() {
+    }
 
     /**
      * @return the columnas
@@ -43,16 +47,37 @@ public class Tablero {
         this.filas = filas;
     }
 
-    public void mostrarTablero() {
-        tablero = new char[getFilas()][getColumnas()];
-        
+    public void crearTamanio() {
+        mostrar_tablero = new String[getFilas()][getColumnas()];
         int fila, columna;
-        for (fila = 0; fila < filas; fila++) {
-            for (columna = 0; columna < columnas; columna++) {
-                System.out.print("|_");
-                tablero[fila][columna] = ' ';
-                System.out.print(tablero[fila][columna]);
-                System.out.print("_");
+        for (fila = this.filas - 1; fila >= 0; fila--) {
+            for (columna = 0; columna < this.columnas; columna++) {
+                mostrar_tablero[fila][columna] = " ";
+            }
+        }
+    }
+
+    public void agregarSubidas(int ix, int iy, int fx, int fy) {
+            mostrar_tablero[iy][ix] = "+";
+    }
+
+    public void agregarBajones(int ix, int iy, int fx, int fy) {
+        mostrar_tablero[iy][ix] = "-";
+    }
+
+    public void crearTablero() {
+        int fila, columna;
+        for (fila = this.filas - 1; fila >= 0; fila--) {
+            for (columna = 0; columna < this.columnas; columna++) {
+                if (fila % 2 == 0) {
+                    System.out.print("|_");
+                    System.out.print(mostrar_tablero[fila][columna]);
+                    System.out.print("_");
+                } else {
+                    System.out.print("|_");
+                    System.out.print(mostrar_tablero[fila][columna]);
+                    System.out.print("_");
+                }
             }
             System.out.println("|");
         }
