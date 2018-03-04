2673f3e86aa56b36efe0b663608103918e68e05d
diego <diego@192.168.0.21>
2018-03-04 16:40:42 -0600

    Practica1 en proceso

--- src/ipc1/practica1_201503384/Subidas.java
+++ src/ipc1/practica1_201503384/Subidas.java
@@ -11,64 +11,34 @@ package ipc1.practica1_201503384;
  */
 public class Subidas {
 
-    private int inicio_x;
-    private int inicio_y;
-    private int final_x;
-    private int final_y;
-
     /**
-     * @return the inicio_x
+     * @return the inicio
      */
-    public int getInicio_x() {
-        return inicio_x;
+    public int getInicio() {
+        return inicio;
     }
 
     /**
-     * @param inicio_x the inicio_x to set
+     * @param inicio the inicio to set
      */
-    public void setInicio_x(int inicio_x) {
-        this.inicio_x = inicio_x;
+    public void setInicio(int inicio) {
+        this.inicio = inicio;
     }
 
     /**
-     * @return the inicio_y
+     * @return the finaliza
      */
-    public int getInicio_y() {
-        return inicio_y;
+    public int getFinaliza() {
+        return finaliza;
     }
 
     /**
-     * @param inicio_y the inicio_y to set
+     * @param finaliza the finaliza to set
      */
-    public void setInicio_y(int inicio_y) {
-        this.inicio_y = inicio_y;
+    public void setFinaliza(int finaliza) {
+        this.finaliza = finaliza;
     }
 
-    /**
-     * @return the final_x
-     */
-    public int getFinal_x() {
-        return final_x;
-    }
-
-    /**
-     * @param final_x the final_x to set
-     */
-    public void setFinal_x(int final_x) {
-        this.final_x = final_x;
-    }
-
-    /**
-     * @return the final_y
-     */
-    public int getFinal_y() {
-        return final_y;
-    }
-
-    /**
-     * @param final_y the final_y to set
-     */
-    public void setFinal_y(int final_y) {
-        this.final_y = final_y;
-    }
+    private int inicio;
+    private int finaliza;
 }
