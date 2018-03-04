8781c8686e976bc91743feb056ef07ed85023e6a
diego <diego@192.168.0.21>
2018-03-03 19:44:12 -0600

    Practica1 en proceso

--- src/ipc1/practica1_201503384/Tablero.java
+++ src/ipc1/practica1_201503384/Tablero.java
@@ -13,10 +13,17 @@ public class Tablero {
 
     private int columnas;
     private int filas;
-    Subidas[] subida;
+    String[] subida;
     private String[][] mostrar_tablero;
+    private int[][] posiciones_tablero;
+    private int contador;
+    private int[] posicion_actual;
+    private String[] simbolo;
 
     public Tablero() {
+        contador = 1;
+        posicion_actual = new int[4];
+        simbolo = new String[4];
     }
 
     /**
@@ -49,16 +56,25 @@ public class Tablero {
 
     public void crearTamanio() {
         mostrar_tablero = new String[getFilas()][getColumnas()];
+        posiciones_tablero = new int[getFilas()][getColumnas()];
         int fila, columna;
         for (fila = this.filas - 1; fila >= 0; fila--) {
             for (columna = 0; columna < this.columnas; columna++) {
-                mostrar_tablero[fila][columna] = " ";
+                if (fila % 2 == 0) {
+                    mostrar_tablero[fila][columna] = " ";
+                    posiciones_tablero[fila][columna] = contador;
+                    contador++;
+                } else {
+                    mostrar_tablero[fila][columna] = " ";
+                    posiciones_tablero[fila][this.columnas - 1 - columna] = contador;
+                    contador++;
+                }
             }
         }
     }
 
     public void agregarSubidas(int ix, int iy, int fx, int fy) {
-            mostrar_tablero[iy][ix] = "+";
+        mostrar_tablero[iy][ix] = "+";
     }
 
     public void agregarBajones(int ix, int iy, int fx, int fy) {
@@ -67,7 +83,7 @@ public class Tablero {
 
     public void crearTablero() {
         int fila, columna;
-        for (fila = this.filas - 1; fila >= 0; fila--) {
+        for (fila = 0; fila < this.filas; fila++) {
             for (columna = 0; columna < this.columnas; columna++) {
                 if (fila % 2 == 0) {
                     System.out.print("|_");
@@ -82,5 +98,23 @@ public class Tablero {
             System.out.println("|");
         }
         System.out.println("");
+        contador = 1;
+    }
+
+    public void agregarJugador(String simbolo, int posiciones, int numero_jugador) {
+        int fila, columna;
+        System.out.println(numero_jugador);
+        if (this.simbolo[numero_jugador] == null) {
+            this.simbolo[numero_jugador] = simbolo;
+        }
+
+        posicion_actual[numero_jugador] = posiciones + posicion_actual[numero_jugador];
+        for (fila = 0; fila < this.filas; fila++) {
+            for (columna = 0; columna < this.columnas; columna++) {
+                if (posiciones_tablero[fila][columna] == posicion_actual[numero_jugador]) {
+                    mostrar_tablero[fila][columna] = this.simbolo[numero_jugador];
+                }
+            }
+        }
     }
 }
